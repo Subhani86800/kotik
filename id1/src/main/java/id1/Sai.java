7ebd7730f1115a1487c5b55b19de6753def75cd2
koti <kkr.kothapally@gmail.com>
2016-08-11 13:20:05 +0530

    initial commit

--- id1/src/main/java/id1/Sai.java
+++ id1/src/main/java/id1/Sai.java
@@ -5,10 +5,18 @@ public class Sai {
 	public static void main(String[] args) {
 		class Dance{
 			public double add(double a,double b){
-				return a+b;
+				double result= a+b;
+				return result;
 			}
-	
+				public double sub(double a,double b){
+					double result = a-b;
+					return result;
 }
+					public double mul(double a,double b){
+						double result= a*b;
+						return result;
 	}
 
+}
+	}
 }
